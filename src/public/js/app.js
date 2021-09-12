a787096f969b4d8400c631534adc515d62072a5a
hyongti <hyongtiii@gmail.com>
2021-09-12 20:53:01 +0900

    #0.4 Recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,2 @@
+// frontEnd
 alert("hi");
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
+// backEnd
 import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public")); // 이 라인이 public 폴더를 유저에게 공개
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
