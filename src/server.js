c1aa06e80283859023a7af27e51b7c86965b414f
hyongti <hyongtiii@gmail.com>
2021-09-13 13:42:37 +0900

    #2.1 Installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,47 +1,48 @@
 // backEnd
 import express from "express";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import SocketIO from "socket.io";
 import http from "http";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public")); // 이 라인이 public 폴더를 유저에게 공개
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// app.listen(3000, handleListen);
 
-// http와 ws를 동시에 작동(?)시킬 수 있음.
-const server = http.createServer(app);
-// http 서버 위에 webSocket 서버를 만듦.
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+const wsServer = SocketIO(httpServer);
 
-const onSocketClose = () => {
-  console.log("Disconnected from the Browser ❌"); // 내(Server) 터미널에 출력됨
-};
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "anonymous";
-  console.log("Connected to Browser ✅");
-  socket.on("close", onSocketClose);
-  socket.on("message", (message) => {
-    // => 여깄는 메시지는 닉네임이나 진짜 메시지나 구분을 못함.
-    const msgObject = JSON.parse(message); // string 을 js object로 바꿔줌.
-    switch (msgObject.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${msgObject.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = msgObject .payload;
-    }
-  });
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(3000, handleListen);
+// const onSocketClose = () => {
+//   console.log("Disconnected from the Browser ❌");
+// };
+
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "anonymous";
+//   console.log("Connected to Browser ✅");
+//   socket.on("close", onSocketClose);
+//   socket.on("message", (message) => {
+//     const msgObject = JSON.parse(message);
+//     switch (msgObject.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname} : ${msgObject.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = msgObject.payload;
+//     }
+//   });
+// });
+
+httpServer.listen(3000, handleListen);
