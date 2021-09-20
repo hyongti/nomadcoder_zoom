3391e2055dcbac9076f063b25f5f8895fc3c9e58
hyongti <hyongtiii@gmail.com>
2021-09-20 12:30:28 +0900

    #2.10 User Count

--- src/server.js
+++ src/server.js
@@ -33,6 +33,10 @@ function publicRooms() {
   return publicRooms;
 }
 
+function countRoom(roomName) {
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "anonymous";
   socket.onAny((event) => {
@@ -41,12 +45,12 @@ wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname); // 메시지를 하나의 소켓에만
+    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // 메시지를 하나의 소켓에만
     wsServer.sockets.emit("room_change", publicRooms()); // 메시지를 모든 소켓에
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname)
+      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
     );
   });
   socket.on("disconnect", () => {
